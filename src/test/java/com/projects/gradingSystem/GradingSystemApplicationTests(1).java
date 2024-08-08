6e74af935f814774750c77c5fb299544ac74e726
camicitro <camilacitro1106@gmail.com>
2024-08-08 16:36:30 -0300

    Estructura

---
+++ src/test/java/com/projects/gradingSystem/GradingSystemApplicationTests(1).java
@@ -0,0 +1,13 @@
+package com.projects.gradingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
